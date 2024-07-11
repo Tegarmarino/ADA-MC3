70ed2891d7b2ba4e557216c536c83fb2540eeeb7
Tegar marino <lelurnaningtegarolga@gmail.com>
2024-07-11 14:43:50 +0700

    Initial Commit

---
+++ Nocjurnal-ADA-MC3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Nocjurnal-ADA-MC3
+//
+//  Created by Tegar marino on 11/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Nocjurnal-ADA-MC3/Nocjurnal_ADA_MC3App.swift
@@ -0,0 +1,17 @@
+//
+//  Nocjurnal_ADA_MC3App.swift
+//  Nocjurnal-ADA-MC3
+//
+//  Created by Tegar marino on 11/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Nocjurnal_ADA_MC3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
