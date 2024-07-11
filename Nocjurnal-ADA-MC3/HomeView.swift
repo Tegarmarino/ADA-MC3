3aec1034550ac3ecd8da4d4f1b6a66ccb6f88d90
Tegar marino <lelurnaningtegarolga@gmail.com>
2024-07-11 15:17:54 +0700

    Initial commit

---
+++ Nocjurnal-ADA-MC3/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  Nocjurnal-ADA-MC3
+//
+//  Created by Tegar marino on 11/07/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Home View")
+    }
+}
+
+#Preview {
+    HomeView()
+}
--- Nocjurnal-ADA-MC3/Nocjurnal_ADA_MC3App.swift
+++ Nocjurnal-ADA-MC3/Nocjurnal_ADA_MC3App.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct Nocjurnal_ADA_MC3App: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+//            ContentView()
+            HomeView()
         }
     }
 }
