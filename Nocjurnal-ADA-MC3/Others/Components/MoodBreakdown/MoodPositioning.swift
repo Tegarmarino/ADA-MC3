360f466d73ff9351474907469a31e956d71fc4a9
ndylanl <dylanlienardi@gmail.com>
2024-07-15 16:49:58 +0700

    Attempt at the mood breakdown and temporary pie chart implementation

---
+++ Nocjurnal-ADA-MC3/Others/Components/MoodBreakdown/MoodPositioning.swift
@@ -0,0 +1,18 @@
+//
+//  MoodPositioning.swift
+//  Nocjurnal-ADA-MC3
+//
+//  Created by Nicholas Dylan Lienardi on 15/07/24.
+//
+
+import SwiftUI
+
+struct MoodPositioning: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MoodPositioning()
+}
