26b2021206bb0b78f9f148c8e90b05bedc59b947
jessiline <jimanuela@student.ciputra.ac.id>
2024-07-23 20:59:57 +0700

    func getcolor di reportview pindah ke folder others>functions krna dipake lagi di page lain jd kupisah file aja. buat card detailview

---
+++ Nocjurnal-ADA-MC3/View/Report/CardDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  CardDetailView.swift
+//  Nocjurnal-ADA-MC3
+//
+//  Created by jessiline imanuela on 23/07/24.
+//
+
+import SwiftUI
+
+struct CardDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardDetailView()
+}
