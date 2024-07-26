214e4edd70254d4b3913a260914e891ecbab6cdb
Marino <94583539+Tegarmarino@users.noreply.github.com>
2024-07-26 14:27:46 +0700

    Final integrasi logical Shop dan juga Tampilan homeview

---
+++ Nocjurnal-ADA-MC3/View/Inventory/InventoryViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  InventoryViewModel.swift
+//  Nocjurnal-ADA-MC3
+//
+//  Created by Tegar marino on 25/07/24.
+//
+
+import Foundation
---
+++ Nocjurnal-ADA-MC3/View/User/InventoryRow.swift
@@ -0,0 +1,18 @@
+//
+//  InventoryRow.swift
+//  Nocjurnal-ADA-MC3
+//
+//  Created by Tegar marino on 23/07/24.
+//
+
+import SwiftUI
+
+struct InventoryRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InventoryRow()
+}
